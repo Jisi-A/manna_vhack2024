7a44dfdb8d34eb54894b48395a42b519375a7c2f
JohnasLeee <johaslee97@gmail.com>
2024-03-19 00:41:28 +0800

    :sparkle: Added VERY POLITE CHATBOT

--- lib/pages/ai_assistant.dart
+++ lib/pages/ai_assistant.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:manna_vhack2024/components/custom_appbar.dart';
 
@@ -10,16 +11,67 @@ class _ai_assistantState extends State<ai_assistant> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: TAppBar(
-        title: 'AI Assistant',
-        isIcon: true,
-      ),
-      body: Center(
-        child: Text(
-          'AI_assistant page',
-          style: TextStyle(fontSize: 24),
+        appBar: TAppBar(
+          title: 'AI Expert',
+          isIcon: true,
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 25,
+                    backgroundImage: AssetImage('assets/ai.png'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    child: Text('AI Expert',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15)),
+                  )
+                ],
+              ),
+              Text(
+                  'As a plant and crop expert, I’m here to assist you with any questions related to plants, gardening, and agriculture. Feel free to ask, and let’s cultivate some green knowledge together! 🌱🌿🌻'),
+              SizedBox(
+                height: 10,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  child: Text('You',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+                ),
+              ),
+              Text(
+                  'My area had a flood for a few days and my seedlings are ruined, what should I do?'),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 25,
+                    backgroundImage: AssetImage('assets/ai.png'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    child: Text('AI Expert',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15)),
+                  )
+                ],
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text("You gonna cry? The fuck you want me to do?"),
+              ),
+            ],
+          ),
+        ));
   }
 }
