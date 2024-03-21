42166c64b67d497a2083bbe1b952b61077b43bcb
Jisi-A <7216lyc@gmail.com>
2024-03-21 19:37:57 +0800

    ➖remove unused dependency

--- lib/pages/account.dart
+++ lib/pages/account.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:settings_ui/settings_ui.dart';
 import 'package:manna_vhack2024/components/custom_appbar.dart';
 
 class account extends StatefulWidget {
